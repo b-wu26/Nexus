0ed105714f9d4c404f35bcb1083bdb996bd33416
b-wu26 <brenden.wu@gmail.com>
2024-02-25 01:07:08 -0500

    model change for schema update

--- Backend/Models/student_profile.py
+++ Backend/Models/student_profile.py
@@ -4,10 +4,14 @@ from . import db
 class student_profile(db.Model):
     idstudent_profile = db.Column(db.Integer, primary_key=True)
     waterloo_id = db.Column(db.String(50), nullable=False,unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     account_password = db.Column(db.String(50))
     f_name = db.Column(db.String(50), nullable=False)
     l_name = db.Column(db.String(50), nullable=False)
     validated = db.Column(db.Boolean)
+    bio = db.Column(db.String(50), nullable=True)
+    term = db.Column(db.String(50), nullable=True)
+    major = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
         return f"<student_profile {self.f_name}>"
@@ -15,6 +19,7 @@ class student_profile(db.Model):
     def __init__(
         self,
         waterloo_id,
+        email,
         account_password,
         f_name,
         l_name,
@@ -22,6 +27,7 @@ class student_profile(db.Model):
     ):
         self.waterloo_id = waterloo_id
         self.account_password = account_password
+        self.email = email
         self.f_name = f_name
         self.l_name = l_name
         self.validated = validated
